33184491fec72b67a987c3c3a22b971eaa9a0d7d
Corey Handelsman <chandelsman@gmail.com>
2021-04-19 15:17:06 -0600

    More testing

--- src/test-script.R
+++ src/test-script.R
@@ -1,6 +1,18 @@
 
 
+library(tidyverse)
+
 # Import raw data
-aus_raw <- readxl::read_excel(
-  here::here("data", "AUS 1st Quarter 2021 .xlsx")
-)
+
+aus_raw <-
+  list.files(
+    path = here::here("data"),
+    pattern = "^\\d{4}q\\d-aus\\.xls",
+    full.names = TRUE
+  ) %>%
+  sapply(readxl::read_excel, simplify = FALSE) %>%
+  bind_rows() %>% 
+  mutate(
+    Released_Date = lubridate::mdy(Released_Time), 
+    Original_Complete_Date = lubridate::mdy_hm(Original_Complete_Date)
+  )
